be09506e1ad52433447d5b149d4ca05d0f505a2a
Michael Nishimura <arbitrarity@gmail.com>
2018-04-23 17:09:35 -0400

    Initial commit, styling mostly complete for homepage

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-// Your code here ...
+
+var activeCircle = "circle1";
+
+$( document ).ready(function() {
+    $(".circle").on("click", function() {
+      var elem = $(this);
+      var clickedId = elem.attr("id");
+      var idNum = clickedId.slice(-1);
+      var bgUrl = "http://lorempixel.com/1100/400/city/"+idNum
+      if (activeCircle !== clickedId){
+        $("#"+activeCircle).addClass("circleFilled");
+        activeCircle = clickedId;
+        $("#"+clickedId).removeClass("circleFilled");
+        $("#background").css("background", "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+bgUrl+")");
+      }
+    })
+});
